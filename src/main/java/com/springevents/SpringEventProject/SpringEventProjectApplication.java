3b799c59174f7c5ea6cf471de3205b44bdcf5c4f
adamjsmith15 <ajsmith015@gmail.com>
2019-06-12 10:20:38 -0400

    Initial Commit

---
+++ src/main/java/com/springevents/SpringEventProject/SpringEventProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springevents.SpringEventProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEventProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEventProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springevents/SpringEventProject/SpringEventProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springevents.SpringEventProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringEventProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
